5f80b16e84072b33f634d4538afe2e5bf0a0ee91
Rafael Lopes <rafaellopes@pop-os.bbrouter>
2024-07-12 18:49:01 -0300

    feat(hero): create hero section

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,14 @@
+export default function Hero() {
+  return (
+    <div className="bg-gradient-to-r from-cyan-700 to-blue-700 py-16">
+      <div className="container mx-auto flex flex-col gap-2 px-2">
+        <h1 className="text-5xl text-white font-bold">
+          Encontre a sua próxima aventura
+        </h1>
+        <p className="text-2xl text-white">
+          Temos ofertas exclusivas para que você se divirta gastando pouco
+        </p>
+      </div>
+    </div>
+  );
+}
