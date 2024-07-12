cdcf4721f77cfabaf0057a6cbad142b6de2a41ad
Rafael Lopes <rafaellopes@pop-os.bbrouter>
2024-07-12 17:21:46 -0300

    feat(header): create header component and rename title in

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
         <span className="flex space-x-2">
           <Link
             to="/sign-in"
-            className="flex items-center text-blue-600 px-3 font-bold hover:bg-gray-100"
+            className="flex items-center text-blue-600 px-3 font-bold bg-white rounded hover:bg-gray-100"
           >
             Entrar
           </Link>
