c6b51d203ced1d36808a034c5de62bb660946b13
rafael <rafaellopes.dev@gmail.com>
2024-08-08 10:02:49 -0300

    feat(register): integrate useNavigate hook in Register.tsx
    Redirect users to the home page upon successful registration.

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -2,6 +2,7 @@ import { useForm } from 'react-hook-form';
 import { useMutation } from 'react-query';
 import * as apiClient from '../api-client';
 import { useAppContext } from '../contexts/AppContext';
+import { useNavigate } from 'react-router-dom';
 
 export type RegisterFormData = {
   firstName: string;
@@ -12,6 +13,7 @@ export type RegisterFormData = {
 };
 
 export default function Register() {
+  const navigate = useNavigate();
   const { showToast } = useAppContext();
   const {
     register,
@@ -23,6 +25,7 @@ export default function Register() {
   const mutation = useMutation(apiClient.register, {
     onSuccess: () => {
       showToast({ message: 'Registro realizado', type: 'SUCCESS' });
+      navigate('/');
     },
     onError: (error: Error) => {
       showToast({ message: error.message, type: 'ERROR' });
