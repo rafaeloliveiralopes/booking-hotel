19ae933089842e7db9fd9b5b153d225a45c8a26a
rafael <rafaellopes.dev@gmail.com>
2024-08-08 13:32:29 -0300

    feat(auth): add userId to Request interface and implement verifyToken middleware
    Enhance the Express Request interface to include userId for authentication purposes.
    Implement verifyToken middleware to validate user tokens and attach userId to requests.

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,7 +1,19 @@
 import { NextFunction, Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 
-const verifyToken = (req: Request, res: Response, next: NextFunction) => {
+declare global {
+  namespace Express {
+    interface Request {
+      userId: string;
+    }
+  }
+}
+
+export const verifyToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   const token = req.cookies['auth_token'];
   if (!token) {
     return res.status(401).json({ message: 'unauthorized' });
@@ -9,6 +21,8 @@ const verifyToken = (req: Request, res: Response, next: NextFunction) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY as string);
+    req.userId = (decoded as JwtPayload).userId;
+    next();
   } catch (error) {
     return res.status(401).json({ message: 'unauthorized' });
   }
